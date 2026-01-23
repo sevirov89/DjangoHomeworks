f7f3aca680163a304a211810a3924dcb9bfd04dd
dsevirov <sevirovdiman@mail.ru>
2026-01-23 14:50:29 +0300

    add files

---
+++ 1.1-first-project/first_project/app/views.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    file_list = os.listdir()
+    files = '\n'.join(file_list)
+    msg = f'Список файлов в рабочей директории:\n{files}'
+    return HttpResponse(msg)
